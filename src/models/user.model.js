0af3731dd92ac02f352e6a4480229435c22a07b9
2230226 <adityatiwari.smn@gmail.com>
2025-02-02 01:07:03 +0530

    git commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,4 +50,47 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save",async function (next){
+  if(!this.isModified("password")) return next(); 
+
+  this.password = brcrypt.hash(this.password, 10)
+  
+
+   next()
+
+userSchema.methods.isPasswordCorrect = async function(password){
+
+ return await bcrypt.compare(password, this.password)
+
+
+}
+userSchema.methods.generateAccessToken=function(){
+ return Jwt.sign(
+  {
+    _id: this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname
+
+  },
+process.env.ACCESS_TOKEN_SECRET,
+{
+  expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+
+})
+}
+userSchema.methods.generateRefreshToken=function()
+{
+  return Jwt.sign(
+    {
+      _id: this._id
+    },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  
+  })
+}
+
+})
 export const User = mongoose.model("User", userSchema);
